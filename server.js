ecd047a2d6cfd0169846b54190f2a8006097f644
suvayan-sdg <164470123+suvayan-sdg@users.noreply.github.com>
2025-05-13 17:08:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello, World!</h1>");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
